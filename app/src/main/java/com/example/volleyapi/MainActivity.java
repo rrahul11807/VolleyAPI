d54226e3a5c49246e38617d9299fa1bafbbfff28
Rahul <rrahul.rajappan@gmail.com>
2019-06-06 16:09:15 +1200

    Getting error

--- app/src/main/java/com/example/volleyapi/MainActivity.java
+++ app/src/main/java/com/example/volleyapi/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.volleyapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url="https://anapioficeandfire.com/api/characters/583";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+
+
+
+            }
 }
